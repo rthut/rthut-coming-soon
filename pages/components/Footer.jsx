a294ef525b99b8d6b11fca02bb224339d6d6e3d5
rthut <rthutsolutions@gmail.com>
2021-12-13 23:25:43 +0530

    first commit

---
+++ pages/components/Footer.jsx
@@ -0,0 +1,61 @@
+import {
+  Box,
+  Container,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  Flex,
+  Tag,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+
+const Logo = (props) => {
+  return (
+    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+        RTHUT
+    </Text>
+  );
+};
+
+const ListHeader = ({ children }) => {
+  return (
+    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+export default function LargeWithLogoCentered() {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      
+      <Box py={10}>
+        <Flex
+          align={'center'}
+          _before={{
+            content: '""',
+            borderBottom: '1px solid',
+            borderColor: useColorModeValue('gray.200', 'gray.700'),
+            flexGrow: 1,
+            mr: 8,
+          }}
+          _after={{
+            content: '""',
+            borderBottom: '1px solid',
+            borderColor: useColorModeValue('gray.200', 'gray.700'),
+            flexGrow: 1,
+            ml: 8,
+          }}>
+          <Logo />
+        </Flex>
+        <Text pt={6} fontSize={'sm'} textAlign={'center'}>
+          © 2021 RTHUT Solutions. All rights reserved.
+        </Text>
+      </Box>
+    </Box>
+  );
+}
